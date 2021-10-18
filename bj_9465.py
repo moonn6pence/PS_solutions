1fb8b935344a5afc7a1dde38071beef260da27a3
moonn6pence <shin421179@gmail.com>
2021-10-18 21:54:08 +0900

    211018 commmit

--- bj_9465.py
+++ bj_9465.py
@@ -4,3 +4,14 @@ for _ in range(case):
     board = []
     board.append(list(map(int, input().split())))
     board.append(list(map(int, input().split())))
+    dp = [[0 for i in range(n)] for _ in range(2)]
+    dp[0][0] = board[0][0]
+    dp[1][0] = board[1][0]
+    for i in range(1, n):
+        if i == 1:
+            dp[0][i] = dp[1][i-1]+board[0][i]
+            dp[1][i] = dp[0][i-1]+board[1][i]
+        else:
+            dp[0][i] = max(dp[1][i-2]+board[0][i], dp[1][i-1]+board[0][i])
+            dp[1][i] = max(dp[0][i-2]+board[1][i], dp[0][i-1]+board[1][i])
+    print(max(dp[0][n-1], dp[1][n-1]))
