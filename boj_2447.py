8fde687bce5cbade4cd7645d6db4efdb2f8631a4
moonn6pence <shin421179@gmail.com>
2021-11-03 22:18:40 +0900

    211103 boj 2448

--- boj_2447.py
+++ boj_2447.py
@@ -1,26 +1,26 @@
 def fractal(n):
-    matrix=[]
-    
+    matrix = []
+
     for i in range(len(n)*3):
-        if i//len(n)==1:
-            matrix.append(n[i%len(n)]+" "*len(n)+n[i%len(n)])
+        if i//len(n) == 1:
+            matrix.append(n[i % len(n)]+" "*len(n)+n[i % len(n)])
         else:
-            matrix.append(n[i%len(n)]*3)
+            matrix.append(n[i % len(n)]*3)
     return(matrix)
-            
-    
-base=['***','* *','***']
 
-n=int(input())
 
-k=0
+base = ['***', '* *', '***']
 
-while n!=3:
-    n=n//3
-    k+=1
+n = int(input())
+
+k = 0
+
+while n != 3:
+    n = n//3
+    k += 1
 
 for i in range(k):
-    base=fractal(base)
+    base = fractal(base)
 
 for _ in base:
     print(_)
