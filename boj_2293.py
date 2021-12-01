5a943807bebdb6eb5d9f75486f56c7c9b670d05d
moonn6pence <shin421179@gmail.com>
2021-12-01 15:56:56 +0900

    211201 boj 2293

--- boj_2293.py
+++ boj_2293.py
@@ -1,19 +1,14 @@
-n,k=map(int,input().split())
-coinList=[]
-for _ in range(n):
-  coinList.append(int(input()))
+import sys
+input = sys.stdin.readline
 
-dp=[0]*(k+1)
+n, k = map(int, input().split())
+coin = [int(input()) for _ in range(n)]
 
+dp = [0]*(k+1)
+dp[0] = 1
 
-def recur(total,w):
-  for i in range(total):
-    if(w-coinList[i]>0):
-      dp[w]+=recur(total,w-coinList[i])
-    elif(w==coinList[i]):
-      dp[w]+=1
+for i in range(n):
+    for j in range(coin[i], k+1):
+        dp[j] += dp[j-coin[i]]
 
-  return dp[w]
-
-print(dp)
-# print(recur(n,k))
+print(dp[k])
