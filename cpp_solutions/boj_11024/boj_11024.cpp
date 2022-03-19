476b96561b59f09624c51c9325752261e7f52ed1
moonn6pence <shin421179@gmail.com>
2022-03-19 17:30:58 +0900

    220319 boj 11024

---
+++ cpp_solutions/boj_11024/boj_11024.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+  fastio;
+  int t;
+  cin >> t;
+  cin.ignore();
+  while (t--)
+  {
+    string n;
+    int sum = 0;
+    getline(cin, n);
+    string tmp = "";
+    int nlen = n.size();
+    for (int i = 0; i < nlen;i++){
+      tmp += n[i];
+      if(n[i]==' '|| i == nlen-1){
+        sum += stoi(tmp);
+        tmp = "";
+      }
+    }
+    cout << sum << '\n';
+  }
+}
