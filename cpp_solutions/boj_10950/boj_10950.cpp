c86b82e2fa0ea4e1e8544b712d7fbb3dc075a233
moonn6pence <shin421179@gmail.com>
2022-03-19 17:31:08 +0900

    220319 boj 10950

---
+++ cpp_solutions/boj_10950/boj_10950.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+  fastio;
+  int t = 0;
+  cin >> t;
+  int a, b;
+  while(t--){
+    cin >> a >> b;
+    cout << a + b << '\n';
+  }
+}
