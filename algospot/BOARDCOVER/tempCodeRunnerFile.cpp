e924a8ac15d12305f9168856d71be3182bd6c023
moonn6pence <shin421179@gmail.com>
2022-07-17 12:35:35 +0900

    remove debug file

--- algospot/BOARDCOVER/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-        // cout << "k : " << k << '\n';
-        // for (int i = 0; i < h;i++){
-        //   for (int j = 0; j < w;j++){
-        //     cout << visited[i][j];
-        //   }
-        //   cout << '\n';
-        // }
