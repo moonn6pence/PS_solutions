4446fadd38c4783bb9c375481557fa3dd0e6107b
moonn6pence <shin421179@gmail.com>
2023-01-12 19:52:06 +0900

    boj 15480  230112 LCA와 쿼리
    - LCA, sparse table, 트리
    - sparse table 응용 LCA
    - 루트가 달라도 트리의 특성을 이용하면 루트 안바꾸면서 쿼리 해결 가능함.
    - lca(r,u,v) = max(level[lca(u,v), level[lca(r,u)], level[lca(r,v)]) 의 특징을 갖음

---
+++ cpp_solutions/boj_15480/boj_15480.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+int N, M, maxLevel;
+int parent[100001][18], level[100001];
+vector<int> adj[100001];
+
+void input(){
+    cin >> N;
+    
+    int a, b;
+    
+    for (int i = 1; i < N; i++){
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+}
+
+void setTree(int node, int pnode, int lv){
+    parent[node][0] = pnode;
+    level[node] = lv;
+    
+    for (int i = 1; i <= maxLevel; i++){
+        parent[node][i] = parent[parent[node][i - 1]][i - 1];
+    }
+    
+    for (int i = 0; i < adj[node].size(); i++){
+        int child = adj[node][i];
+        if (child == pnode) continue;
+        setTree(child, node, lv + 1);
+    }
+}
+
+
+int LCA(int a, int b){
+    if (a == 1 || b == 1) return 1;
+    
+    if (level[a] < level[b]) swap(a, b);
+    
+    while (level[a] != level[b]){
+        for (int i = maxLevel; i >= 0; i--){
+            if (level[parent[a][i]] >= level[b])
+                a = parent[a][i];
+        }
+    }
+    
+    int ret = a;
+    
+    if (a != b){
+        for (int i = maxLevel; i >= 0; i--){
+            if (parent[a][i] != parent[b][i]){
+                a = parent[a][i];
+                b = parent[b][i];
+            }
+            
+            ret = parent[a][i];
+        }
+    }
+    
+    return ret;
+}
+
+
+void solve(){
+    cin >> M;
+    
+    int r, u, v;
+    while (M--){
+        cin >> r >> u >> v;
+        int ac1 = LCA(r, u);
+        int ac2 = LCA(r, v);
+        int ac3 = LCA(u, v);
+        if (level[ac1] < level[ac2]) swap(ac1, ac2);
+        if (level[ac1] < level[ac3]) swap(ac1, ac3);
+        
+        cout << ac1 << '\n';
+    }
+}
+
+void solution(){
+    input();
+    maxLevel = (int)floor(log2(100001));
+    setTree(1, 0, 1);
+    solve();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solution();
+}
