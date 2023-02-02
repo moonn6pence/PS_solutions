6515678c699a682e9ba06dec8edff13401a6669e
moonn6pence <shin421179@gmail.com>
2023-02-02 17:56:08 +0900

    boj 17141  230202 연구소 2 cpp
    - BFS, 구현
    - 17142와 시리즈 문제

---
+++ cpp_solutions/boj_17141/boj_17141.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+typedef pair<int, int> pii;
+const int INF = 1e9;
+int N, M;
+int graph[50][50];
+vector<pii> virus;
+int visited[10][50][50];
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
+
+void input() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> graph[i][j];
+			if (graph[i][j] == 2) virus.push_back({ i, j });
+		}
+	}
+}
+
+void BFS(int src) {
+	queue<pii> order;
+	order.push(virus[src]);
+	visited[src][virus[src].first][virus[src].second] = 0;
+
+	while (!order.empty()) {
+		int x = order.front().first, y = order.front().second;
+		order.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i], ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+			if (visited[src][nx][ny] != -1 || graph[nx][ny] == 1) continue;
+
+			order.push({ nx, ny });
+			visited[src][nx][ny] = visited[src][x][y] + 1;
+		}
+	}
+}
+
+void unionTime(vector<vector<int>>& time, int cur) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (time[i][j] > visited[cur][i][j] && visited[cur][i][j] != -1) {
+				time[i][j] = visited[cur][i][j];
+			}
+		}
+	}
+}
+
+
+int overlapBFS(int state) {
+	vector<vector<int>> time(50, vector<int>(50, INF));
+	int ret = 0;
+
+	for (int i = 0; i < virus.size(); i++) {
+		if (state & (1 << i)) {
+			unionTime(time, i);
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+
+			if (graph[i][j] == 0 && time[i][j] == INF) {
+				return INF;
+			}
+
+			if (graph[i][j] == 0 || graph[i][j] == 2) {
+				ret = max(ret, time[i][j]);
+			}
+		}
+	}
+
+	return ret;
+}
+
+int solve(int state, int firstFree, int cnt) {
+
+	if (cnt == M) {
+		return overlapBFS(state);
+	}
+
+	int ret = INF;
+
+	for (int i = firstFree; i < virus.size(); i++) {
+		if ((state & (1 << i))) continue;
+
+		ret = min(ret, solve(state | (1 << i), i + 1, cnt + 1));
+	}
+
+	return ret;
+}
+
+void solution() {
+	input();
+	fill(&visited[0][0][0], &visited[virus.size() - 1][N - 1][N], -1);
+	for (int i = 0; i < virus.size(); i++) BFS(i);
+	int answer = solve(0, 0, 0);
+	answer = (answer < INF) ? answer : -1;
+	cout << answer;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+
+	solution();
+}
