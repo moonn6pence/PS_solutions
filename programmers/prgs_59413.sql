65f65794ecc0af5db5556c422f8db1d88e173071
moonn6pence <shin421179@gmail.com>
2023-10-13 02:28:21 +0900

    prgs 59413 입양 시각 구하기(2) sql
    - SET 구문으로 변수 선언 가능. 변수명 앞에는 @를 붙여야함. (사용할 때도)
    - `:=` 은 대입 연산자. set으로 선언한 변수에 대입 가능.
    - := 이걸 응용해서 @x := @x + 1 과 where @x < 12 을 같이 사용해서 for문처럼 사용가능

---
+++ programmers/prgs_59413.sql
@@ -0,0 +1,11 @@
+-- 입양 시각 구하기(2)
+SET @HOUR = -1;
+SELECT
+    (@HOUR := @HOUR + 1) as "HOUR",
+    (
+        SELECT COUNT(HOUR(DATETIME))
+        FROM ANIMAL_OUTS
+        WHERE HOUR(DATETIME) = @HOUR
+    ) as "COUNT"
+FROM ANIMAL_OUTS
+WHERE @HOUR < 23;
